2d9bd962459a5e809ca7796cfa4197a7facc138e
Chris Villarreal <chrisvillarreal1018@gmail.com>
2021-08-29 04:26:10 -0500

    mobile updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
+import Row from 'react-bootstrap/Row';
 import qs from 'query-string';
+import is from 'is_js';
 
 import { getAssets } from './fetch';
 
@@ -20,53 +22,58 @@ const App = () => {
 			<h3 style={{ textAlign: 'center', color: '#ffffff' }}>
 				assets for{' '}
 				<a href={`https://opensea.io/accounts/${account}`}>
-					{account.substring(0, 4)}...
-					{account.substring(account.length - 4)}
+					{is.mobile()
+						? `${account.substring(0, 4)}...${account.substring(
+								account.length - 4
+						  )}`
+						: account}
 				</a>
 			</h3>
 			<ul></ul>
-			<Table striped bordered hover variant="dark" style={{ margin: '.5rem' }}>
-				<thead>
-					<tr>
-						<th>name</th>
-						<th>top offer</th>
-						<th>cheapest listing</th>
-						<th>last sale</th>
-					</tr>
-				</thead>
-				<tbody>
-					{assets.map(
-						({
-							id,
-							name,
-							link,
-							topOffer,
-							cheapestListing,
-							lastSale,
-						}) => (
-							<tr key={id}>
-								<td>
-									<a href={link} target="_blankn">
-										{name}
-									</a>
-								</td>
-								<td>{topOffer}</td>
-								<td>{cheapestListing}</td>
-								<td>{lastSale}</td>
-							</tr>
-						)
-					)}
-					<tr>
-						<td colSpan="4" />
-					</tr>
-					<tr>
-						<td>totals</td>
-						<td>{totals.topOffer}</td>
-						<td>{totals.cheapestListing}</td>
-						<td>{totals.lastSale}</td>
-					</tr>
-				</tbody>
-			</Table>
+			<Row style={{ margin: '0.5rem' }}>
+				<Table striped bordered hover variant="dark">
+					<thead>
+						<tr>
+							<th>name</th>
+							<th>top offer</th>
+							<th>cheapest listing</th>
+							<th>last sale</th>
+						</tr>
+					</thead>
+					<tbody>
+						{assets.map(
+							({
+								id,
+								name,
+								link,
+								topOffer,
+								cheapestListing,
+								lastSale,
+							}) => (
+								<tr key={id}>
+									<td>
+										<a href={link} target="_blankn">
+											{name}
+										</a>
+									</td>
+									<td>{topOffer}</td>
+									<td>{cheapestListing}</td>
+									<td>{lastSale}</td>
+								</tr>
+							)
+						)}
+						<tr>
+							<td colSpan="4" />
+						</tr>
+						<tr>
+							<td>totals</td>
+							<td>{totals.topOffer}</td>
+							<td>{totals.cheapestListing}</td>
+							<td>{totals.lastSale}</td>
+						</tr>
+					</tbody>
+				</Table>
+			</Row>
 		</>
 	);
 };
